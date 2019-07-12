eaee358af8459937dfbf845a2336443b0a15f8a2
Oleksii Maksymov <maksimov98gmy@gmail.com>
2019-07-12 16:04:40 +0300

    Versio 3.0 (added message chooser, which allows the aim programm for the message)

--- app/src/main/java/com/hfad/mymesseger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymesseger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -13,9 +14,15 @@ public class CreateMessageActivity extends Activity {
         setContentView(R.layout.activity_create_message);
     }
 
-    public void onSendMessage (View view) {
-    Intent intent = new Intent(this, ReceiveMessageActivity.class);
-    startActivity(intent);
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        //Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/hfad/mymesseger/ReceiveMessageActivity.java
+++ app/src/main/java/com/hfad/mymesseger/ReceiveMessageActivity.java
@@ -1,14 +1,21 @@
 package com.hfad.mymesseger;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class ReceiveMessageActivity extends AppCompatActivity {
+public class ReceiveMessageActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.message);
+        messageView.setText(messageText);
     }
 }
